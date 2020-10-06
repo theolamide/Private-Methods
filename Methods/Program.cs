62680a9011b9c5c9e324270b203797e60b8a68fd
theolamide <ajadidan@gmail.com>
2020-10-05 23:34:12 -0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ReverseString()
+        {
+            string message = "Hello World!";
+        }
+    }
+}
